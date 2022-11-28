f45174b1033b6c19aa9b526400b4d61fdd6ced35
HamedSadim1 <hamid.sadim@outlook.com>
2022-11-28 14:12:17 +0100

    Home page: opmaak en styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,118 @@
-// Stap 1: Importeer React
 import * as React from "react"
 import Layout from "../components/layout"
-import { StaticImage } from "gatsby-plugin-image"
-// Stap 2: definieer je component
-const IndexPage = () => {
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Artist from "../components/artist"
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  headerTitle,
+  CTA,
+  section,
+  subtitle,
+  artists,
+} from "../page.module.css"
+
+const IndexPage = ({
+  data: {
+    wpPage: { homeFields },
+  },
+}) => {
+  const image = getImage(homeFields.picture.localFile)
+
   return (
-    <Layout pageTitle="Welcome to Hamed Agency">
-      <p>Lorem ipsum</p>
-      <StaticImage
-        width="800"
-        height="600"
-        alt="randomized unplash image!"
-        src="../images/girl-foto.jpg"
-      />
+    <Layout>
+      <section className={header}>
+        <article className={headerInfo}>
+          <h1 className={headerTitle}>{homeFields.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: homeFields.description,
+            }}
+          />
+          <a
+            className={CTA}
+            target="__blank"
+            href={homeFields.callToAction.url}
+          >
+            {homeFields.callToAction.title}
+            Book an Artist
+          </a>
+        </article>
+        <div>
+          <GatsbyImage
+            image={image}
+            className={headerPicture}
+            alt={homeFields.picture.altText}
+          />
+        </div>
+      </section>
+      <section className={section}>
+        <h2 className={subtitle}>Featured Artists</h2>
+        <p>
+          description Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed doo eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className={artists}>
+          {homeFields.artists.map(artist => {
+            return (
+              <Artist
+                slug={`artists/${artist.slug}`}
+                key={artist.id}
+                artist={artist}
+              />
+            )
+          })}
+        </div>
+      </section>
     </Layout>
   )
 }
-// Stap 3: Exporteer je component
+
+export const query = graphql`
+  query {
+    wpPage(slug: { eq: "home" }) {
+      homeFields {
+        artists {
+          ... on WpArtist {
+            id
+            slug
+            artistMeta {
+              artistName
+              firstName
+              lastName
+              profilePicture {
+                altText
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(
+                      placeholder: BLURRED
+                      transformOptions: { grayscale: true }
+                    )
+                  }
+                }
+              }
+            }
+          }
+        }
+        callToAction {
+          title
+          url
+        }
+        title
+        description
+        picture {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 export default IndexPage
